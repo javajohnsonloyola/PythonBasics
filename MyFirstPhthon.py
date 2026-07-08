83f2812cf2ec417462a4c74a4226979d3535426e
JOHNSON J <javajohnsonloyola@gmail.com>
2026-07-08 15:24:29 +0200

    first commit

---
+++ MyFirstPhthon.py
@@ -0,0 +1,10 @@
+def say_hi(name):
+    if name == '':
+        print("You didn't enter your name!")
+    else:
+        print("Hi there...")
+    
+    for letter in name:
+        print(letter)
+
+say_hi("erik")
